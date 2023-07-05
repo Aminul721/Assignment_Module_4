a2aa17afa54584d2fc16e7c048d082124bbd49f4
Aminul Islam <41473410+Aminul721@users.noreply.github.com>
2023-07-05 13:39:20 +0600

    Add files via upload

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,19 +1,21 @@
 let cartItems = [];
 
 export function addToCart(product, quantity = 1) {
-  const cartItem = cartItems.find(item => item.product.id === product.id);
-
-  if (cartItem) {
-    cartItem.quantity += quantity;
-  } else {
-    cartItems.push({ product, quantity });
-  }
+    const cartItem = cartItems.find(item => item.product.id === product.id);
+    //   console.log(cartItem, "cartItem match")
+    if (cartItem) {
+        cartItem.quantity += quantity;
+    } else {
+        cartItems.push({ product, quantity });
+    }
+    //   console.log(cartItems, "CartItems array")
 }
 
+
 export function getCartItems() {
-  return cartItems;
+    return cartItems;
 }
 
 export function clearCart() {
-  cartItems = [];
+    cartItems = [];
 }
